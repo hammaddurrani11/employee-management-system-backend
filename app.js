52399980f5fdcac1d09c05ae812d3f14b5b07ea4
Hammad Durrani <hammaddurrani11@gmail.com>
2026-01-27 17:20:38 +0500

    Remove deprecated mongoose connection options from connectToDB function

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ async function connectToDB() {
         console.log('MONGO_URI exists:', !!process.env.MONGO_URI);
         
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             maxPoolSize: 1,
             minPoolSize: 1,
             serverSelectionTimeoutMS: 5000,
